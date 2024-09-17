13f6dc3fdbeab7070392fffb8a80f2877e0974c5
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-09-17 10:59:24 +0800

    add purescript toolchain to dev environment

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     devenv.url = "github:cachix/devenv";
+    easy-purescript-nix.url = "github:justinwoo/easy-purescript-nix";
   };
 
   outputs = {
@@ -10,12 +11,14 @@
     nixpkgs,
     devenv,
     flake-utils,
+    easy-purescript-nix,
     ...
   } @ inputs:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
       };
+      easy-ps = easy-purescript-nix.packages.${system};
     in {
       devShells.default = devenv.lib.mkShell {
         inherit inputs pkgs;
@@ -35,13 +38,28 @@
             packages = with pkgs;
               [
                 exercism
+
+                # nix
                 alejandra
+
+                # go
                 gofumpt
+
+                # purescript
+                nodejs_22
+                esbuild
               ]
               ++ (with pkgs.haskellPackages; [
                 hoogle
                 hindent
                 hlint
+              ])
+              ++ (with easy-ps; [
+                purs-0_15_8
+                spago
+                purescript-language-server
+                purs-tidy
+                purescript-psa
               ]);
           }
         ];
