d690c7eaba4c91ccd932b2760529bee35485e62f
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2025-01-17 14:44:00 +0800

    add solution for binary-search in rust

---
+++ rust/binary-search/src/lib.rs
@@ -0,0 +1,24 @@
+pub fn find(xs: &[i32], key: i32) -> Option<usize> {
+    find_(xs, key, 0, xs.len())
+}
+
+fn find_(xs: &[i32], key: i32, lo: usize, hi: usize) -> Option<usize> {
+    let len = hi - lo;
+    if len == 0 {
+        return None;
+    }
+    let mid = (hi + lo) / 2;
+
+    if len % 2 == 0 {
+        match key.cmp(&xs[mid]) {
+            std::cmp::Ordering::Less => find_(xs, key, lo, mid),
+            _ => find_(xs, key, mid, hi),
+        };
+    }
+
+    match key.cmp(&xs[mid]) {
+        std::cmp::Ordering::Equal => Some(mid),
+        std::cmp::Ordering::Less => find_(xs, key, lo, mid),
+        std::cmp::Ordering::Greater => find_(xs, key, mid + 1, hi),
+    }
+}
