f35a27e4f1400b1cd7ba7a433084701ac00599c5
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2025-12-08 11:32:34 +0800

    partial solution for decimal now with sign support

--- rust/decimal/src/lib.rs
+++ rust/decimal/src/lib.rs
@@ -1,16 +1,22 @@
 use std::{
+    cmp::Ordering,
     collections::VecDeque,
     fmt::Display,
     ops::{Add, Mul, Sub},
 };
 
+use itertools::Itertools;
+
 /// Type implementing arbitrary-precision decimal arithmetic
-#[derive(Debug, PartialEq, PartialOrd)]
+#[derive(Debug, PartialEq)]
 pub struct Decimal {
     /// Least-significant-limb
     significand: Vec<u8>,
     /// Canonical form, minimum scale of 1
     scale: usize,
+
+    /// Sign bit
+    is_negative: bool,
 }
 
 impl Decimal {
@@ -19,15 +25,20 @@ impl Decimal {
             return None;
         };
 
-        let significand = Decimal::extract_significand_digits(input)?;
+        let (significand, is_negative) =
+            Decimal::extract_significand_and_sign(input)?;
         let scale = Decimal::determine_scale(input);
 
-        let decimal = Decimal::normalize(significand, scale);
+        let decimal = Decimal::normalize(significand, scale, is_negative);
 
         Some(decimal)
     }
 
-    fn normalize(significand: Vec<u8>, scale: usize) -> Decimal {
+    fn normalize(
+        significand: Vec<u8>,
+        scale: usize,
+        is_negative: bool,
+    ) -> Decimal {
         // Remove trailing zeros
         let mut moving_scale = scale;
         let mut significand = VecDeque::from(significand);
@@ -44,25 +55,34 @@ impl Decimal {
         }
 
         // Remove leading zeros
-        let mut rev_significand_iter =
-            significand.clone().into_iter().rev().peekable();
-        while let Some(&leading) = rev_significand_iter.peek() {
-            if leading.ne(&0) {
-                break;
-            }
-            if leading.eq(&0) && significand.len() > 2 {
-                significand.pop_back();
-            }
-            rev_significand_iter.next();
+        let rev_significand_iter =
+            significand.clone().into_iter().rev().tuple_windows();
+
+        for (leading, next_leading) in rev_significand_iter {
+            match (leading, next_leading) {
+                (0, 0) if { significand.len() > 2 } => significand.pop_back(),
+                (0, _) if { significand.len() > 2 } => significand.pop_back(),
+                _ => {
+                    break;
+                }
+            };
         }
+
+        // Invariant on canonical form
+        // Example: zero is Decimal { [0, 0], 1, false }
+        assert!(significand.len() >= 2);
+        assert!(moving_scale >= 1);
 
         Decimal {
             significand: significand.into_iter().collect(),
             scale: moving_scale,
+            is_negative,
         }
     }
 
-    fn extract_significand_digits(input: &str) -> Option<Vec<u8>> {
+    fn extract_significand_and_sign(input: &str) -> Option<(Vec<u8>, bool)> {
+        // very rudimentary check
+        let is_negative = input.contains('-');
         let input_iter = input.chars().rev();
         let mut significand = VecDeque::new();
 
@@ -71,7 +91,7 @@ impl Decimal {
                 '0'..='9' => {
                     significand.push_back(c.to_digit(10).unwrap() as u8)
                 }
-                '.' => {}
+                '.' | '-' | '+' => {}
                 _ => return None,
             }
         }
@@ -82,7 +102,7 @@ impl Decimal {
             significand.push_front(0);
         }
 
-        Some(significand.into())
+        Some((significand.into(), is_negative))
     }
 
     fn determine_scale(input: &str) -> usize {
@@ -108,7 +128,59 @@ impl Display for Decimal {
         display.insert(self.scale, '.');
 
         let display = display.iter().rev().cloned().collect::<String>();
-        write!(f, "{}", display)
+        write!(f, "{}{}", if self.is_negative { "-" } else { "" }, display)
+    }
+}
+
+impl PartialOrd for Decimal {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let sign_compare = self.is_negative.cmp(&other.is_negative);
+        if sign_compare.eq(&Ordering::Greater) {
+            return Some(Ordering::Less);
+        } else if sign_compare.eq(&Ordering::Less) {
+            return Some(Ordering::Greater);
+        }
+
+        let (
+            self_denormalized_significand_msb,
+            other_denormalized_significand_msb,
+        ): (Vec<u8>, Vec<u8>) = match self.scale.cmp(&other.scale) {
+            Ordering::Greater => (
+                self.significand
+                    .clone()
+                    .into_iter()
+                    .chain(vec![0; self.scale.sub(other.scale)])
+                    .rev()
+                    .collect(),
+                vec![0; self.scale.sub(other.scale)]
+                    .iter()
+                    .rev()
+                    .chain(&other.significand)
+                    .copied()
+                    .collect(),
+            ),
+            Ordering::Less => (
+                vec![0; other.scale.sub(self.scale)]
+                    .iter()
+                    .rev()
+                    .chain(&self.significand)
+                    .copied()
+                    .collect(),
+                other.significand
+                    .clone()
+                    .into_iter()
+                    .chain(vec![0; other.scale.sub(self.scale)])
+                    .rev()
+                    .collect(),
+            ),
+            _ => (
+                self.significand.clone().into_iter().rev().collect(),
+                other.significand.clone().into_iter().rev().collect(),
+            ),
+        };
+
+        self_denormalized_significand_msb
+            .partial_cmp(&other_denormalized_significand_msb)
     }
 }
 
