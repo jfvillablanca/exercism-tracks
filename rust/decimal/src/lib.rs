0d8e17216f311e344cbea2353144ee7556245a55
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2025-12-12 12:28:03 +0800

    implement addition for positive decimals; fix normalization again lmao

--- rust/decimal/src/lib.rs
+++ rust/decimal/src/lib.rs
@@ -2,6 +2,7 @@ use std::{
     cmp::Ordering,
     collections::VecDeque,
     fmt::Display,
+    iter::zip,
     ops::{Add, Mul, Sub},
 };
 
@@ -12,7 +13,7 @@ use itertools::Itertools;
 pub struct Decimal {
     /// Least-significant-limb
     significand: Vec<u8>,
-    /// Canonical form, minimum scale of 1
+    /// Canonical form, minimum scale of 0
     scale: usize,
     /// Sign bit
     is_negative: bool,
@@ -39,15 +40,15 @@ impl Decimal {
         is_negative: bool,
     ) -> Decimal {
         // Remove trailing zeros
-        let mut moving_scale = scale;
+        let mut frac_len = scale;
         let mut significand = VecDeque::from(significand);
         let mut significand_iter = significand.clone().into_iter().peekable();
         while let Some(&trailing) = significand_iter.peek() {
             if trailing.ne(&0) {
                 break;
             }
-            if trailing.eq(&0) && moving_scale > 1 {
-                moving_scale = moving_scale.saturating_sub(1);
+            if trailing.eq(&0) && frac_len >= 1 {
+                frac_len = frac_len.saturating_sub(1);
                 significand.pop_front();
             }
             significand_iter.next();
@@ -59,22 +60,27 @@ impl Decimal {
 
         for (leading, next_leading) in rev_significand_iter {
             match (leading, next_leading) {
-                (0, 0) if significand.len() > moving_scale => {
+                (0, 0) if significand.len() > frac_len => {
                     significand.pop_back()
                 }
-                (0, x) if x != 0 && significand.len() > 2 => {
+                (0, x) if x != 0 && significand.len() > frac_len => {
                     significand.pop_back()
                 }
-                (0, _) if moving_scale == 1 => significand.pop_back(),
+                (0, _) if frac_len == 0 => significand.pop_back(),
                 _ => {
                     break;
                 }
             };
         }
 
+        // If only zero is left, we represent zero as an empty vec
+        if significand.len() == 1 && significand.contains(&0) {
+            significand.pop_back();
+        }
+
         Decimal {
             significand: significand.into_iter().collect(),
-            scale: moving_scale,
+            scale: frac_len,
             is_negative,
         }
     }
@@ -119,7 +125,7 @@ impl Decimal {
         lhs: &Decimal,
         rhs: &Decimal,
         is_msb: bool,
-    ) -> (Vec<u8>, Vec<u8>) {
+    ) -> (Vec<u8>, Vec<u8>, (usize, usize)) {
         let max_frac = lhs.scale.max(rhs.scale);
         let min_frac = lhs.scale.min(rhs.scale);
         let max_int = (lhs.significand.len() - lhs.scale)
@@ -129,6 +135,11 @@ impl Decimal {
 
         let right_pad = vec![0; max_frac - min_frac];
         let left_pad = vec![0; max_int - min_int];
+        let pad_for_zero_value_decimal = if !left_pad.is_empty() {
+            left_pad.clone()
+        } else {
+            right_pad.clone()
+        };
 
         let lhs_rev: Vec<u8> = lhs.significand.iter().copied().rev().collect();
         let rhs_rev: Vec<u8> = rhs.significand.iter().copied().rev().collect();
@@ -145,14 +156,31 @@ impl Decimal {
             Ordering::Greater => {
                 let lhs_out = [left_pad.clone(), lhs_rev.clone()].concat();
                 let rhs_out = [rhs_rev.clone(), right_pad.clone()].concat();
-                (apply_rev(lhs_out), apply_rev(rhs_out))
+                assert_eq!(lhs_out.len(), rhs_out.len());
+                (apply_rev(lhs_out), apply_rev(rhs_out), (max_frac, max_int))
             }
             Ordering::Less => {
                 let lhs_out = [lhs_rev.clone(), right_pad.clone()].concat();
                 let rhs_out = [left_pad.clone(), rhs_rev.clone()].concat();
-                (apply_rev(lhs_out), apply_rev(rhs_out))
+                assert_eq!(lhs_out.len(), rhs_out.len());
+                (apply_rev(lhs_out), apply_rev(rhs_out), (max_frac, max_int))
             }
-            Ordering::Equal => (apply_rev(lhs_rev), apply_rev(rhs_rev)),
+            Ordering::Equal => {
+                // Handles zero-length significands,
+                // i.e. zero-valued Decimals compared with integrals
+                let lhs_out = if lhs_rev.is_empty() {
+                    pad_for_zero_value_decimal.clone()
+                } else {
+                    lhs_rev
+                };
+                let rhs_out = if rhs_rev.is_empty() {
+                    pad_for_zero_value_decimal
+                } else {
+                    rhs_rev
+                };
+                assert_eq!(lhs_out.len(), rhs_out.len());
+                (apply_rev(lhs_out), apply_rev(rhs_out), (max_frac, max_int))
+            }
         }
     }
 }
@@ -184,6 +212,7 @@ impl PartialOrd for Decimal {
         let (
             self_denormalized_significand_msb,
             other_denormalized_significand_msb,
+            _,
         ) = Decimal::denormalize_for_operation(self, other, true);
         match (
             self_denormalized_significand_msb
@@ -202,8 +231,22 @@ impl PartialOrd for Decimal {
 impl Add for Decimal {
     type Output = Self;
 
-    fn add(self, _rhs: Self) -> Self::Output {
-        todo!()
+    fn add(self, rhs: Self) -> Self::Output {
+        let (lhs, rhs, (max_frac, _min_int)) =
+            Decimal::denormalize_for_operation(&self, &rhs, false);
+
+        let mut carry_over = 0;
+        let mut sum = Vec::new();
+        for (lh, rh) in zip(lhs, rhs) {
+            let x = (lh + rh) % 10;
+            sum.push(x + carry_over);
+            carry_over = (lh + rh) / 10;
+        }
+        if carry_over != 0 {
+            sum.push(carry_over)
+        };
+
+        Decimal::normalize(sum, max_frac, false)
     }
 }
 
